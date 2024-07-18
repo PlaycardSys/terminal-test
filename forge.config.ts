7bf92706847b94f7e0b8649178034987ce641bb5
Daniel Scapulatempo <danielscapulatempo@gmail.com>
2024-07-18 14:54:06 -0300

    update forge settings

--- forge.config.ts
+++ forge.config.ts
@@ -9,6 +9,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
   packagerConfig: {
+    executableName: "terminal-consulta",
     name: 'Terminal de Consulta',
     asar: true,
   },
